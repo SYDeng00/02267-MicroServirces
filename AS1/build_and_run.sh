a466d2d7028c48dadb02dda74311719b27c62187
SY.Dan <70686119+SYDeng00@users.noreply.github.com>
2024-01-07 20:05:03 +0100

    add docker-compose shell

---
+++ AS1/build_and_run.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+pushd code-with-quarkus
+mvn package
+# Create a new docker image if necessary.
+docker-compose build
+# Restarts the container with the new image if necessary
+docker-compose up -d
+# The server stays running.
+# To terminate the server run docker-compose down in the
+# code-with-quarkus direcgtory
+# clean up images
+docker image prune -f 
+popd
+
+# Give the Web server a chance to finish start up
+sleep 2 
+
+pushd demo_client
+mvn test
+popd
