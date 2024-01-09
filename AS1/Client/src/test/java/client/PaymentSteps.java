adb7630ad24c0068fac6ce57fb210dfba25527ab
SY.Dan <70686119+SYDeng00@users.noreply.github.com>
2024-01-09 10:13:02 +0100

    Update PaymentSteps.java

--- AS1/Client/src/test/java/client/PaymentSteps.java
+++ AS1/Client/src/test/java/client/PaymentSteps.java
@@ -3,6 +3,8 @@ package client;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.math.BigDecimal;
+
 import org.acme.Controllers.GeneralController;
 import org.acme.Models.Customer;
 import org.acme.Models.Trade;
@@ -22,56 +24,55 @@ public class PaymentSteps {
 	public int merchant_Balance;
 	public int balance;
 	public ClientService clientService = new ClientService();
-    CallBankAuthService callBankAuthService = new CallBankAuthService();
-    GeneralController generalController= new GeneralController();
-    CustomersService customerService= new CustomersService();
+	CallBankAuthService callBankAuthService = new CallBankAuthService();
+	GeneralController generalController = new GeneralController();
+	CustomersService customerService = new CustomersService();
 
-    public static Customer customer;
-    public static Customer merchant;
-    public static String caccountID;
-    public static String maccountID;
-    public static String transRT;
+	public static Customer customer;
+	public static Customer merchant;
+	public static String caccountID;
+	public static String maccountID;
+	public static String transRT;
 
 	@Given("a customer with a bank account with balance {int}")
 	public void aCustomerWithABankAccountWithBalance(Integer int1) throws BankServiceException_Exception {
 		CallBankAuthService callBankAuthService = new CallBankAuthService();
-        
-        customer=new Customer("AAAA","BBBB","CCCC", int1.doubleValue(), "customer");
-        customer_Account=callBankAuthService.CreateOneAccount(customer);
-        System.out.println(customer_Account);
-        System.out.println(customer);
-        
-   
+
+		customer = new Customer("AAAAA", "BBBBB", "CCCCC", int1.doubleValue(), "customer");
+		customer_Account = callBankAuthService.CreateOneAccount(customer);
+		System.out.println(customer_Account);
+		System.out.println(customer);
+
 	}
 
 	@Given("that the customer is registered with DTU Pay")
 	public void thatTheCustomerIsRegisteredWithDTUPay() throws BankServiceException_Exception {
 		customer.setBankAccount(customer_Account);
-        caccountID = clientService.regDTUUser(customer);
-        System.out.println(caccountID);
+		caccountID = clientService.regDTUUser(customer);
+		System.out.println(caccountID);
 //        assertTrue(true, "The customer is registered");
 	}
 
 	@Given("a merchant with a bank account with balance {int}")
 	public void aMerchantWithABankAccountWithBalance(Integer int1) throws BankServiceException_Exception {
-		merchant=new Customer("DDDD","EEEE","FFFF", int1.doubleValue(), "customer");
-        merchant_Account =callBankAuthService.CreateOneAccount(merchant);
-        System.out.println(merchant);
+		merchant = new Customer("DDDDD", "EEEEE", "FFFFF", int1.doubleValue(), "customer");
+		merchant_Account = callBankAuthService.CreateOneAccount(merchant);
+		System.out.println(merchant);
 //        assertTrue(true, "The bank account is created for the customer");
 	}
 
 	@Given("that the merchant is registered with DTU Pay")
 	public void thatTheMerchantIsRegisteredWithDTUPay() throws BankServiceException_Exception {
 		merchant.setBankAccount(merchant_Account);
-        maccountID = clientService.regDTUUser(merchant);
-        System.out.println(maccountID);
+		maccountID = clientService.regDTUUser(merchant);
+		System.out.println(maccountID);
 //        assertTrue(true, "The customer is registered");
 
 	}
 
 	@When("the merchant initiates a payment for {int} kr by the customer")
 	public void theMerchantInitiatesAPaymentForKrByTheCustomer(Integer int1) {
-		transRT = clientService.transferMoney(new Trade(customer_Account, merchant_Account,int1.doubleValue()));
+		transRT = clientService.transferMoney(new Trade(customer_Account, merchant_Account, int1.doubleValue()));
 	}
 
 	@Then("the payment is successful")
@@ -81,22 +82,21 @@ public class PaymentSteps {
 
 	@Then("the balance of the customer at the bank is {int} kr")
 	public void theBalanceOfTheCustomerAtTheBankIsKr(Integer int1) throws BankServiceException_Exception {
-		int balance=(int) generalController.getBalance(customer_Account).doubleValue();
 
+		BigDecimal balance = callBankAuthService.checkBalance(customer_Account);
 //        generalController.deleteAccount(customeraccountID);
-        assertEquals(int1,balance);
+		assertEquals(int1, balance.intValue());
 	}
 
 	@Then("the balance of the merchant at the bank is {int} kr")
 	public void theBalanceOfTheMerchantAtTheBankIsKr(Integer int1) throws BankServiceException_Exception {
-		int balance=(int) generalController.getBalance(merchant_Account).doubleValue();
-//      generalController.deleteAccount(customeraccountID);
-      assertEquals(int1,balance);
+		BigDecimal balance = callBankAuthService.checkBalance(merchant_Account);
+		assertEquals(int1, balance);
 	}
+
 	@After
 	public void afterScenario() {
 		clientService.deleteAccount(customer_Account);
 		clientService.deleteAccount(merchant_Account);
 	}
 }
-
