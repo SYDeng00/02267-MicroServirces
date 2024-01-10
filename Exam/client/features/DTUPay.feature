681d901f37fd1352799b69d070758255b14c1b72
SY.Dan <70686119+SYDeng00@users.noreply.github.com>
2024-01-10 18:22:25 +0100

    Token Success

--- Exam/client/features/DTUPay.feature
+++ Exam/client/features/DTUPay.feature
@@ -3,12 +3,12 @@
 Feature: Account
 
   Scenario: Create customer DTUPay account 
-    Given the customer "First_C0000" "Last_C0000" with CPR "1230000" with balance 1000
+    Given the customer "First_C00010" "Last_C00010" with CPR "12300010" with balance 1000
     When the customer has registered with DTUPay
     Then we receive a customer dtuPayId
-    
+
   Scenario: Create merchant DTUPay account
-    Given the merchant "First_M0000" "Last_M0000" with CPR "45460000" with balance 1000
+    Given the merchant "First_M00010" "Last_M00010" with CPR "454600010" with balance 1000
     When the merchant has registered with DTUPay
     Then we receive a merchant dtuPayId
 
--- Exam/client/src/test/java/payment/DTUPaySteps.java
+++ Exam/client/src/test/java/payment/DTUPaySteps.java
@@ -3,6 +3,8 @@ package payment;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import domin.DTUPayAccount;
 import domin.DTUPay_Interface;
@@ -15,25 +17,28 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class AccountSteps {
-
+public class DTUPaySteps {
 
 	public String customerBankID;
 	public String customerDtuPayID;
-	
+
 	public String merchantBankID;
 	public String merchantDtuPayID;
-	
+
 	public DTUPayAccount dtu_customer;
 	public DTUPayAccount dtu_merchant;
+
+	
+	public List<String> tokens = new ArrayList<>();;
 	
 	
 	DTUPay_Interface dtuPay = new DTUPay_Interface();
 	BankService bank = new BankServiceService().getBankServicePort();
 
 
-	
-	
+	// Account test
+	// Author: Siyuan Deng
+
 	@Given("the customer {string} {string} with CPR {string} with balance {int}")
 	public void the_customer_with_cpr_with_balance(String firstName, String lastName, String cpr, Integer balance)
 			throws BankServiceException_Exception {
@@ -43,9 +48,9 @@ public class AccountSteps {
 		bank_user_customer.setCprNumber(cpr);
 		BigDecimal customerBalance = new BigDecimal(balance);
 		customerBankID = bank.createAccountWithBalance(bank_user_customer, customerBalance);
-        assertFalse(customerBankID.isEmpty());
-		
-        dtu_customer = new DTUPayAccount(firstName, lastName, cpr, customerBankID);
+		assertFalse(customerBankID.isEmpty());
+
+		dtu_customer = new DTUPayAccount(firstName, lastName, cpr, customerBankID);
 	}
 
 	@When("the customer has registered with DTUPay")
@@ -55,11 +60,12 @@ public class AccountSteps {
 
 	@Then("we receive a customer dtuPayId")
 	public void we_receive_a_dtu_pay_id() {
-        assertFalse(customerDtuPayID.isEmpty());
+		assertFalse(customerDtuPayID.isEmpty());
 	}
 
 	@Given("the merchant {string} {string} with CPR {string} with balance {int}")
-	public void the_merchant_with_cpr_with_balance(String firstName, String lastName, String cpr, Integer balance) throws BankServiceException_Exception {
+	public void the_merchant_with_cpr_with_balance(String firstName, String lastName, String cpr, Integer balance)
+			throws BankServiceException_Exception {
 		User bank_user_merchant = new User();
 		bank_user_merchant.setFirstName(firstName);
 		bank_user_merchant.setLastName(lastName);
@@ -79,12 +85,33 @@ public class AccountSteps {
 	public void we_receive_a_merchant_dtu_pay_id() throws BankServiceException_Exception {
 		assertFalse(merchantDtuPayID.isEmpty());
 	}
-	
-    @After
-    public void resetUsers() throws BankServiceException_Exception {
-        if (customerBankID != null) bank.retireAccount(customerBankID);
-        if (merchantBankID != null) bank.retireAccount(merchantBankID);
-    }
 
+	@After
+	public void resetUsers() throws BankServiceException_Exception {
+		if (customerBankID != null)
+			bank.retireAccount(customerBankID);
+		if (merchantBankID != null)
+			bank.retireAccount(merchantBankID);
+	}
+
+	// Token test
+	// Author: Siyuan Deng
+
+	@When("the customer asks for tokens")
+	public void theCustomerAsksForTokens() {
+		try {
+			tokens = dtuPay.getTokens(customerDtuPayID);
+		} catch (Exception e) {
+		    // Handle the exception, e.g., log it
+		    e.printStackTrace();
+		}
+		
+
+	}
+
+	@Then("the customer receives tokens")
+	public void theCustomerReceivesTokens() {
+		assertFalse(tokens.isEmpty());
+	}
 
 }
