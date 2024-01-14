3d99c1c5a3353feef0c8b205bac1fa3773bf0123
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-01-14 23:04:32 +0100

    Payment update

---
+++ Exam/payment_microservice/src/main/java/org/acme/Repositories/PaymentRepository.java
@@ -0,0 +1,68 @@
+package org.acme.Repositories;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import org.acme.Domains.Payment;
+import org.acme.Domains.Refund;
+import org.acme.Interfaces.IPaymentRepository;
+/**
+ * Store all the pending payments and refund.
+ * Add new payments and refund, remove them after the coresponding request finished
+ * 
+ * @author Yingli
+ * @version 1.0
+ * 
+ */
+public class PaymentRepository implements IPaymentRepository{
+    HashMap<UUID,Payment> payments = new HashMap<>();
+    HashMap<UUID,Refund> refunds = new HashMap<>();
+    
+    @Override
+    public void addPayment(Payment payment) {
+        payments.put(payment.getPaymentID(), payment);
+    }
+
+    @Override
+    public Payment getPayment(UUID paymentID) {
+        return payments.get(paymentID);
+    }
+
+    @Override
+    public List<Payment> getPayments() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPayments'");
+    }
+
+    @Override
+    public void removePayment(UUID paymentID) {
+        payments.remove(paymentID);
+    }
+
+    @Override
+    public void addRefund(Refund refund) {
+        refunds.put(refund.getRefundId(),refund);
+    }
+
+    @Override
+    public Refund getRefund(UUID refundID) {
+        return refunds.get(refundID);
+    }
+
+    @Override
+    public List<Refund> getRefunds() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getRefunds'");
+    }
+
+    @Override
+    public void removeRefund(UUID refundID) {
+        refunds.remove(refundID);
+    }
+
+   
+
+    
+    
+}
