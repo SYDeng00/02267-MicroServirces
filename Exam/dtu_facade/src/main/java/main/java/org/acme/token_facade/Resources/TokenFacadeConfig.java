16ccf8cb345e12a70092c881f68abc14097c9c1f
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-01-16 22:14:14 +0100

    Token

--- Exam/dtu_facade/src/main/java/main/java/org/acme/token_facade/Resources/TokenFacadeConfig.java
+++ Exam/dtu_facade/src/main/java/main/java/org/acme/token_facade/Resources/TokenFacadeConfig.java
@@ -4,4 +4,5 @@ public class TokenFacadeConfig {
 
     public static final String RETURN_TOKEN = "return Token";
     public static final String RETURN_TOKEN1 = "return Token1";
+    public static final String SEND_TOKEN_REQUEST = "RequestTokens";
 }
