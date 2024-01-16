18146ea35cb9dbd8049b21fda3736309a9c997af
RachelWealth <74123687+RachelWealth@users.noreply.github.com>
2024-01-16 19:52:08 +0100

    no

--- Exam/TokenManagement/src/main/java/org/acme/Domain/Token.java
+++ Exam/TokenManagement/src/main/java/org/acme/Domain/Token.java
@@ -91,5 +91,3 @@ public class Token {
         this.valid = tokenStatus;
     }
 }
-
-
--- Exam/TokenManagement/src/main/java/org/acme/Resource/TokenConfig.java
+++ Exam/TokenManagement/src/main/java/org/acme/Resource/TokenConfig.java
@@ -4,4 +4,12 @@ public class TokenConfig {
 
     public static final String RETURN_TOKEN = "return Token";
     public static final String RETURN_TOKEN1 = "return Token1";
+    public static String STATUS_EXPIRED = "expired";
+    public static int EXPIRY_DAYS_FOR_TOKEN = 2;//count in days
+    public static int MAX_TOKEN_REQ = 5;
+    public static int MAX_UNUSED_TOKEN = 1;
+    public static int MAX_TOKEN_AVAILABLE = MAX_TOKEN_REQ + MAX_UNUSED_TOKEN;
+    public static final String RECEIVE_TOKEN_VALID = "ValidTokens";
+    public static final String SEND_TOKEN_VALID_RESULT = "ValidResult";
+    
 }
