ca2b9d83de37ccf9cf9944d88b5073a06b5c497c
Dan <750866996@qq.com>
2024-05-28 23:50:00 +0200

    add description

--- Exam/account_microservice/src/main/java/org/acme/Resources/AccountConfig.java
+++
@@ -1,9 +0,0 @@
-package org.acme.Resources;
-
-public class AccountConfig {
-    public static final String REGISTER = "Register";
-    public static final String RETURN_ID = "return ID";
-    public static final String SEND_REQUEST_BANK_ACCOUNTS = "RequestBankAccounts";
-    public static final String RECEIVE_GET_ACCOUNTS = "RequestBankAccounts";
-	public static final String ERROR = "error in check bank ID: not exist";
-}
--- Exam/dtu_facade/src/main/java/main/java/org/acme/token_facade/Resources/TokenFacadeConfig.java
+++
@@ -1,8 +0,0 @@
-package main.java.org.acme.token_facade.Resources;
-
-public class TokenFacadeConfig {
-
-    public static final String SEND_RETURN_TOKEN = "requestTokens";
-    public static final String RECEIVE_RETURN_TOKEN = "sendTokens";
-    public static final String SEND_TOKEN_REQUEST = "RequestTokens";
-}
